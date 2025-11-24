f28908636ac5668682857cf58eacb4b48f767c21
Anupam Kumar Aniket <db5856710@gmail.com>
2025-11-24 23:58:39 +0530

    Update xtimate.py

--- xtimate.py
+++ xtimate.py
@@ -6,5 +6,5 @@ t = int(sys.argv[1])
 s = int(sys.argv[2])
 
 for i in range(t):
-    subprocess.run(['xdotool', 'key', 'Alt+Tab'])
+    subprocess.run(['xdotool', 'key', 'Ctrl+Tab'])
     time.sleep(s)
