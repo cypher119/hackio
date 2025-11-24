5b4dacf060fd5f63755577879ab2e4e7a2c501f3
Anupam Kumar Aniket <db5856710@gmail.com>
2025-11-25 00:13:11 +0530

    Update xtimate.py

--- xtimate.py
+++ xtimate.py
@@ -1,10 +1,12 @@
 import subprocess
 import time
 import sys
+import random
 
 t = int(sys.argv[1])
-s = int(sys.argv[2])
+s = random.randint(30, 60)
 
 for i in range(t):
     subprocess.run(['xdotool', 'key', 'Ctrl+Tab'])
     time.sleep(s)
+    
